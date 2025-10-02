675cb3776ff921d1b77bddc89a1d8c3ad98f27d4
Hitesh Kumar <76029553+Hitesh-Kumar-2001@users.noreply.github.com>
2025-10-02 18:18:27 +0530

    Update SegmentTreeBasic.cpp

--- SegmentTreeBasic.cpp
+++ SegmentTreeBasic.cpp
@@ -80,6 +80,21 @@ int query(int i, int sl, int sr, int l, int r) {
     return segmentOperation(left, right);
 }
 
+
+// for find kth zero or any number
+// given a segment tree in which nodes stores occurences of that number we can get kth in range
+
+int find_kth(int v, int tl, int tr, int k) {
+    if (k > t[v])
+        return -1;
+    if (tl == tr)
+        return tl;
+    int tm = (tl + tr) / 2;
+    if (t[v*2] >= k)
+        return find_kth(v*2, tl, tm, k);
+    else 
+        return find_kth(v*2+1, tm+1, tr, k - t[v*2]);
+}
 void update(int i, int sl, int sr, int pos, int newVal) {
    // leaf case
     if (sl == sr) {
